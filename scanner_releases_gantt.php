a7a5e6f86e0096e9f2eb2e1663967bb8a4426237
Siva Jasthi <siva.jasthi@metrostate.edu>
2019-09-09 17:15:51 -0500

    Structure and Interface of VIsual BOM
    Template, Struture and Interface of Visual BOM app.

---
+++ scanner_releases_gantt.php
@@ -0,0 +1,30 @@
+<?php
+
+  $nav_selected = "SCANNER"; 
+  $left_buttons = "YES"; 
+  $left_selected = "RELEASESGANTT"; 
+
+  include("./nav.php");
+  global $db;
+
+  ?>
+
+
+<div class="right-content">
+    <div class="container">
+
+      <h3 style = "color: #01B0F1;">Scanner -> System Releases Gantt</h3>
+
+    
+        
+        TODO: Gantt Chart Code
+
+        
+
+ <style>
+   tfoot {
+     display: table-header-group;
+   }
+ </style>
+
+  <?php include("./footer.php"); ?>
---
+++ scanner_sbom_list.php
@@ -0,0 +1,18 @@
+<?php
+  $nav_selected = "SCANNER";
+  $left_buttons = "YES";
+  $left_selected = "SOFTWAREBOM";
+
+  include("./nav.php");
+  
+ ?>
+
+ <div class="right-content">
+    <div class="container">
+
+      <h3 style = "color: #01B0F1;">Scanner --> Software BOM </h3>
+
+    </div>
+</div>
+
+<?php include("./footer.php"); ?>
